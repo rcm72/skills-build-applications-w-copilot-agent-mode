45bd339ccc7706c432f6aed7d89ecfbcd8489677
Robert <robert.cmrlec@gmail.com>
2026-01-13 14:09:08 +0000

    Style React components and navigation with Bootstrap for OctoFit Tracker frontend

---
+++ octofit-tracker/frontend/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item"><Link className="nav-link" to="/activities">Activities</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/leaderboard">Leaderboard</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/teams">Teams</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/users">Users</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/workouts">Workouts</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/workouts" element={<Workouts />} />
+          <Route path="/" element={<Activities />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
